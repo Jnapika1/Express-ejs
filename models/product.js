6e2a465345b5638446b52cc965a71a173b865b18
Jnapika1 <jnapika.aish@gmail.com>
2023-05-23 11:35:18 +0530

    added models(ejs)

---
+++ models/product.js
@@ -0,0 +1,32 @@
+const { get } = require('express/lib/response');
+const fs = require('fs');
+const path=require('path');
+
+const p = path.join(path.dirname(process.mainModule.filename), 'data', 'products.json');
+
+const getProductsfromFile=cb=>{
+        fs.readFile(p, (err, fileContent)=>{
+            if(err){
+                return cb([]);
+            }
+            return cb(JSON.parse(fileContent));
+        })
+}
+
+module.exports = class Product{
+    constructor(t){
+        this.title=t;
+    }
+    save(){
+        getProductsfromFile(products=>{
+            products.push(this);
+            fs.writeFile(p, JSON.stringify(products), err=>{
+                console.log(err);
+            })
+        })
+    }
+
+    static fetchAll(cb){
+        getProductsfromFile(cb);
+    }
+}
